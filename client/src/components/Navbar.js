b44d36a203b33f29e93ae2e87bbb8d531c48c811
unknown <nina.labra@gmail.com>
2021-10-19 13:58:05 +0800

    Updated fonts in Sass file, added material ui packages

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,22 @@
 import '../App.scss';
-import { Nav, Navbar, Button, Image } from 'react-bootstrap';
-import Logo from '../pet-pal-logo.png';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Button } from '@material-ui/core';
+import LoginIcon from '@material-ui/icons/AccountCircle';
+// import AssignmentIcon from '@mui/icons-material/Assignment';
+// import Logo from '../pet-pal-logo.png';
 
 
 export default function NavBar() {
     return(
         <Navbar collapseOnSelect expand="lg" fixed="top"  bg="light" variant="light" className="navbar">
             <Navbar.Brand id="navbar-brand" href="#home">
-                <Image
+                {/* <Image
                     alt=""
                     src={Logo}
                     width="35"
                     height="35"
                     className="d-inline-block align-top"
-                />
+                /> */}
                 Pet Pal
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
@@ -24,8 +27,8 @@ export default function NavBar() {
                     <Nav.Link>Pricing</Nav.Link>
                 </Nav>
                 <Nav id="navbar-btn">
-                    <Button variant="outline-info" className="m-1" id="login-btn">Log In</Button>
-                    <Button variant="outline-info" className="m-1">Sign Up</Button>
+                    <Button startIcon={<LoginIcon />} variant="contained" className="m-1 navbar-buttons" id="login-btn">Log In</Button>
+                    <Button variant="contained" className="m-1 navbar-buttons">Sign Up</Button>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
